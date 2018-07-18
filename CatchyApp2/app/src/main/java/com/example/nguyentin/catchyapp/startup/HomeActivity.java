7e42c773bb1de67fd8faced15e91cac456a529c7
Nguyen Giang Vi Son <gnvisons@gmail.com>
2018-07-18 20:45:25 +0700

    Commit clear focus edittext

--- CatchyApp2/app/src/main/java/com/example/nguyentin/catchyapp/startup/HomeActivity.java
+++ CatchyApp2/app/src/main/java/com/example/nguyentin/catchyapp/startup/HomeActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import com.example.nguyentin.catchyapp.R;
@@ -25,6 +26,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         recHotDeal = (RecyclerView) findViewById(R.id.recHotDeal);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        
         list = new ArrayList<>();
         list.add(new TestModel(BitmapFactory.decodeResource(getResources(), R.drawable.test_hot_deal_1), "Cute summer dress HOT SALE UP TO 50%", "YAME Shop"));
         list.add(new TestModel(BitmapFactory.decodeResource(getResources(), R.drawable.test_hot_deal_2), "Cute summer dress HOT SALE UP TO 50%", "YAME Shop"));
